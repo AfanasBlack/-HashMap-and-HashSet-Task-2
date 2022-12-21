f3b97316c3dc5491bccfcb1e83ddb31ce1cae3ce
Alexandr Afanasev <116384401+AfanasBlack@users.noreply.github.com>
2022-12-21 10:41:07 +0300

    Исправления

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -1,9 +1,16 @@
 public class WordsChecker {
 
+    protected String text;
+
     public WordsChecker(String text) {
+        this.text = text;
     }
 
     public boolean hasWord (boolean word) {
-        return word;
+        if (word) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
